261fdd17105086f759f1c6230cbc611ab86f527c
jerryxfl <1072059168@qq.com>
2023-01-19 13:45:53 +0800

    support rtCore 0.1.7

--- request-base/src/main/java/com/jerry/request_base/interfaces/IConfig.kt
+++ request-base/src/main/java/com/jerry/request_base/interfaces/IConfig.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.jerry.rt.core.http.pojo.Request
 import com.jerry.rt.core.http.pojo.Response
 import com.jerry.request_base.annotations.Configuration
+import com.jerry.rt.core.http.pojo.s.IResponse
 
 /**
  * 配置注册类，需要搭配ConfigRegister 注解同时使用
@@ -11,5 +12,5 @@ import com.jerry.request_base.annotations.Configuration
 abstract class IConfig {
     abstract fun init(annotation: Configuration, clazz: Class<*>)
 
-    abstract fun onRequest(context: Context,request: Request,response: Response):Boolean
+    abstract fun onRequest(context: Context,request: Request,response: IResponse):Boolean
 }
